78a786f18088aafd86db69e47f5b8846647f389e
Sebastian <sebastian.samuelsson@elev.ga.ntig.se>
2023-11-05 20:45:03 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner keyb = new Scanner(System.in);
         Tamagotchi tama = new Tamagotchi(keyb.nextLine(), true);
-        System.out.println(tama.getName());
         tama.addChoice();
-        System.out.println(tama.getChoice());
-        tama.choice(keyb.nextInt());
+        while (tama.isAlive()==true) {
+            System.out.println(tama.getChoice());
+            tama.choice(keyb.nextInt());
+        }
     }
 }
--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Tamagotchi {
     private int hunger;
     private int boredom;
-    private ArrayList<String> words = new ArrayList<String>();
-    private boolean isAlive = true;
-    private Random generator;
+    private ArrayList<String> words = new ArrayList<>();
+    private boolean isAlive;
+    private Random generator = new Random();
     public String name;
-    public ArrayList<String> choice = new ArrayList<String>();
+    public ArrayList<String> choice = new ArrayList<>();
 
     public Tamagotchi(String name, boolean isAlive){
         this.isAlive = isAlive;
@@ -18,51 +19,82 @@ public class Tamagotchi {
     public void addChoice(){
         choice.add("1. Feed");
         choice.add("2. Play");
-        choice.add("3. Stats");
+        choice.add("3. Teach word");
+        choice.add("4. Stats");
+        choice.add("5. Hejdå!");
     }
 
     public void choice(int val){
-    if (val == 1){
-        feed();
-    } else if (val == 2) {
-        Hi();
-    } else if (val == 3) {
-        printStats();
-    }
+        Scanner keyb = new Scanner(System.in);
+        switch (val) {
+            case 1 -> feed();
+            case 2 -> Hi();
+            case 3 -> teach(keyb.nextLine());
+            case 4 -> printStats();
+            case 5 -> {System.out.println("Hejdå!"); tick();}
+            default -> System.out.println("Ogiltigt val: " + val);
+        }
     }
 
     public void feed(){
-        hunger--;
+        if (hunger!=0){
+            hunger = hunger-generator.nextInt(10);
+            if(hunger < -1){
+                hunger = -1;
+            }
+        } else if (hunger==0) {
+            System.out.println(name+" is not hungry");
+        }
+        tick();
     }
 
     public void Hi(){
+        int randomItem = generator.nextInt(words.size());
+        String randomWord = words.get(randomItem);
+        System.out.println(randomWord);
         reduceBoredom();
     }
 
     public void teach(String word){
-
+        words.add(word);
+        reduceBoredom();
     }
 
     public void printStats(){
-        System.out.println(hunger);
-        System.out.println(boredom);
-        System.out.println(isAlive);
+        tick();
+        System.out.println("Name: "+name);
+        System.out.println("Hunger: "+hunger);
+        System.out.println("Boredom: "+boredom);
+        isAlive();
+        if (isAlive==true){
+            System.out.println(name+" is alive!");
+        } else if (isAlive==false) {
+            System.out.println(name+" has died... you are an awful parent!");
+        }
     }
 
     public boolean isAlive() {
+        if (hunger==10||boredom==10){
+            isAlive=false;
+        }
         return isAlive;
     }
 
     private void reduceBoredom(){
-        boredom--;
+        if (boredom!=0){
+            boredom = boredom-generator.nextInt(10);
+            if(boredom < -1){
+                boredom = -1;
+            }
+        } else if (boredom==0) {
+            System.out.println(name+" is not bored");
+        }
+        tick();
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    private void tick(){
+        boredom++;
+        hunger++;
     }
 
     public ArrayList<String> getChoice() {
